6dca987124ce81f665c00ed13279f700a6dc7830
Anurag Mishra <anuragkiit@gmail.com>
2020-07-12 12:50:11 +0000

    Added Auth to Add Image

--- Starter Files/ImageGallery/src/ImageGallery.API/Controllers/ImagesController.cs
+++ Starter Files/ImageGallery/src/ImageGallery.API/Controllers/ImagesController.cs
@@ -59,6 +59,7 @@ namespace ImageGallery.API.Controllers
         }
 
         [HttpPost()]
+        [Authorize(Roles = "PayingUser")]
         public IActionResult CreateImage([FromBody] ImageForCreation imageForCreation)
         {
             if (imageForCreation == null)
@@ -97,6 +98,9 @@ namespace ImageGallery.API.Controllers
             // ownerId should be set - can't save image in starter solution, will
             // be fixed during the course
             //imageEntity.OwnerId = ...;
+
+            var ownerId = this.User.Claims.FirstOrDefault(c => c.Type == "sub").Value;
+            imageEntity.OwnerId = ownerId;
 
             // add and save.  
             _galleryRepository.AddImage(imageEntity);
